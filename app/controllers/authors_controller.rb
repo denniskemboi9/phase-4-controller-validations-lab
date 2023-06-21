2c57ecd2bf5ace80c3b1e3a5554a402a779d68f2
DennisKemboi <cherogonydennis9@gmail.com>
2023-06-21 14:56:32 +0300

    Completed Assignment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,5 @@
 class AuthorsController < ApplicationController
-  
+
   def show
     author = Author.find(params[:id])
 
@@ -7,15 +7,24 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
-  end
+
+    # if author.valid?
+    #   render json: author, status: :created
+    # else
+    #   render json: { errors: "Either the author name does not exist or email is invalid" }, status: :unprocessable_entity
+    # end
+
+rescue ActiveRecord::RecordInvalid => invalid
+  render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+end
 
   private
   
   def author_params
     params.permit(:email, :name)
   end
+
   
 end
